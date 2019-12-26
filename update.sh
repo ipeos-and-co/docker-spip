c03123c9fe0e0fa1b930257606902f3f4e5d4314
Laurent Vergerolle - IPEOS <laurent@ipeos.com>
2019-12-26 08:27:19 -0400

    mise à jour critique de sécurité

--- update.sh
+++ update.sh
@@ -13,8 +13,8 @@ declare -A phpVersions=(
 )
 declare -A spipPackages=(
 	[3.0]='3.0.28'
-  [3.1]='3.1.11'
-	[3.2]='3.2.5'
+  [3.1]='3.1.12'
+	[3.2]='3.2.7'
 )
 declare -A mysqlPackages=(
   [3.0]='mysql'
