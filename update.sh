afc259c21f1921e275e4f1f82cb4d333ed665fbb
Laurent Vergerolle - IPEOS <laurent@ipeos.com>
2023-02-27 15:02:00 -0400

    mise à jour faille critique : 4.2.1, 4.1.8, 4.0.10, 3.2.18

--- update.sh
+++ update.sh
@@ -10,8 +10,8 @@ declare -A phpVersions=(
   [4.2]='8.1'
 )
 declare -A spipPackages=(
-	[4.1]='4.1.7'
-	[4.2]='4.2.0'
+	[4.1]='4.1.8'
+	[4.2]='4.2.1'
 )
 declare -A mysqlPackages=(
   [4.1]='mysqli'
