3f382ccd37fd5de0807eaaa4062429c21a914d34
Laurent Vergerolle - IPEOS <laurent@ipeos.com>
2024-12-03 09:03:10 -0400

    MAJ 4.3.5 + FIX (PHP.ini) spip.ini

--- 4.1/docker-entrypoint.sh
+++ 4.1/docker-entrypoint.sh
@@ -42,11 +42,11 @@ fi
 ( \
 echo 'display_errors=Off'; \
 echo 'error_log=/var/log/apache2/php.log'; \
-echo 'max_execution_time=${PHP_MAX_EXECUTION_TIME}'; \
-echo 'memory_limit=${PHP_MEMORY_LIMIT}'; \
-echo 'post_max_size=${PHP_POST_MAX_SIZE}'; \
-echo 'upload_max_filesize=${PHP_UPLOAD_MAX_FILESIZE}'; \
-echo 'date.timezone=${PHP_TIMEZONE}'; \
+echo "max_execution_time=${PHP_MAX_EXECUTION_TIME}"; \
+echo "memory_limit=${PHP_MEMORY_LIMIT}"; \
+echo "post_max_size=${PHP_POST_MAX_SIZE}"; \
+echo "upload_max_filesize=${PHP_UPLOAD_MAX_FILESIZE}"; \
+echo "date.timezone=${PHP_TIMEZONE}"; \
 ) > /usr/local/etc/php/conf.d/spip.ini
 
 
